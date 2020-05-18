cfbbf5ec2891bbf5d546780f83d9723a56823e60
saradajani <37843528+saradajani@users.noreply.github.com>
2020-05-17 17:25:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,49 +14,38 @@ var bb = document.getElementById('bb');
 var art = document.getElementById('art');
 var meh = document.getElementById('meh');
 var ehh = document.getElementById('ehh');
-var uzi = document.getElementById('uzi'); 
-var uzi = document.getElementById('uzi'); 
-var uzi = document.getElementById('uzi'); 
-var uzi = document.getElementById('uzi'); 
+ 
 
-x=1, x=2, x=3, x=4
-run createText
-if x=1
-else
-else if x=2
-What I am showing with algorithm, I am explaining how a function works and is calling other algortihs--> calling reset all algorithm
-Math/logic --> conditional statement
-calling on functions which are defined below
-subabstration example: reset all or stop function (reduced complexity) reduce that to single line of code
-reset all 
-had a problem--> resolved it making code more efficient
-sound playing on eachother, needed a stop function
+function createText (elem) {
+    var uzi = document.getElementById('uzi'); 
+    var bub = document.getElementById('bub'); 
+    var blu = document.getElementById('blu'); 
+    var y = document.getElementById('y');
 
-function createText(elem){
-	resetall();
+    if (uzi.clicked == true) {
+        resetall();
 	a.innerHTML = "A$AP Mob - RAF ft. A$AP Rocky, Playboi Carti, Quavo, Lil Uzi Vert, Frank Ocean";
 	stop();
 	sound.play();
-	
-}
-function createTextt(elem){
-	resetall();
+    } else if (bub.clicked == true) {
+        resetall();
 	b.innerHTML = "A$AP Mob - Yamborghini High (Audio) ft. Juicy J ";
 	stop();
 	soundd.play();
-}
-function createTexttt(elem){
-	resetall();
+    } else if (blu.clicked == true) {
+        resetall();
 	c.innerHTML = "Chief Keef - Blowin Minds ft. A$AP Rocky, Playboi Carti & ASAP Nast";
 	stop();
 	sounddd.play();
-}
-function createTextttt(elem){
+    } else if (y.clicked == true) {
 	resetall();
 	d.innerHTML = "A$AP Rocky - Canal St ft. Bones";
 	stop();
 	soundddd.play();
+    };
 }
+
+
 function spin(elem) {
 	testing.classList.toggle("spin");
 	hah.play();
@@ -75,7 +64,7 @@ function stop() {
 	soundd.pause();
 	sounddd.pause();
 	soundddd.pause();
-1}
+}
 
 function resetall(){
 	a.style.backgroundImage = 'uzi';
